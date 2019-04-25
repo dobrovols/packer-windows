7a3c121c7a991ccff4703bbaea8fbaecce32b37b
Sergey Dobrovolsky <dobrovols@gmail.com>
2019-04-25 15:12:59 +0300

    fix update script logging

--- scripts/win-updates.ps1
+++ scripts/win-updates.ps1
@@ -165,7 +165,7 @@ function Install-WindowsUpdates() {
 function Check-WindowsUpdates() {
     LogWrite "Checking For Windows Updates"
     $Username = $env:USERDOMAIN + "\" + $env:USERNAME
-    LogWrite "Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString()
+    LogWrite ("Script: " + $ScriptPath + "`nScript User: " + $Username + "`nStarted: " + (Get-Date).toString())
 
     $script:UpdateSearcher = $script:UpdateSession.CreateUpdateSearcher()
     $script:successful = $FALSE
