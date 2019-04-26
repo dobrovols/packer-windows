5fb0a65885a1450aa5fff41230c32ee955214f71
Sergey Dobrovolsky <dobrovols@gmail.com>
2019-04-26 14:38:31 +0300

    fix: system is unstable because of ultradefrag

--- scripts/compact.bat
+++ scripts/compact.bat
@@ -7,13 +7,13 @@ if not exist "C:\Windows\Temp\7z1900-x64.msi" (
 )
 msiexec /qb /i C:\Windows\Temp\7z1900-x64.msi
 
-if not exist "C:\Windows\Temp\ultradefrag.zip" (
-	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://downloads.sourceforge.net/project/ultradefrag/stable-release/6.1.0/ultradefrag-portable-6.1.0.bin.amd64.zip', 'C:\Windows\Temp\ultradefrag.zip')" <NUL
-)
+rem if not exist "C:\Windows\Temp\ultradefrag.zip" (
+rem 	powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://downloads.sourceforge.net/project/ultradefrag/stable-release/6.1.0/ultradefrag-portable-6.1.0.bin.amd64.zip', 'C:\Windows\Temp\ultradefrag.zip')" <NUL
+rem )
 
-if not exist "C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64\udefrag.exe" (
-	cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\ultradefrag.zip -oC:\Windows\Temp"
-)
+rem if not exist "C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64\udefrag.exe" (
+rem 	cmd /c ""C:\Program Files\7-Zip\7z.exe" x C:\Windows\Temp\ultradefrag.zip -oC:\Windows\Temp"
+rem )
 
 if not exist "C:\Windows\Temp\SDelete.zip" (
   powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://download.sysinternals.com/files/SDelete.zip', 'C:\Windows\Temp\SDelete.zip')" <NUL
@@ -32,7 +32,7 @@ mkdir C:\Windows\SoftwareDistribution\Download
 net start wuauserv
 
 if "%PACKER_BUILDER_TYPE%" neq "hyperv-iso" (
-	cmd /c C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64\udefrag.exe --optimize --repeat C:
+rem	cmd /c C:\Windows\Temp\ultradefrag-portable-6.1.0.amd64\udefrag.exe --optimize --repeat C:
 
 	cmd /c %SystemRoot%\System32\reg.exe ADD HKCU\Software\Sysinternals\SDelete /v EulaAccepted /t REG_DWORD /d 1 /f
 	cmd /c C:\Windows\Temp\sdelete.exe -q -z C:
